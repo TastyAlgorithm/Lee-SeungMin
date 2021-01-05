d6550fa24e6d141136c5f4a7b5229bb2dacc444b
TastyAlgorithm <76274621+TastyAlgorithm@users.noreply.github.com>
2021-01-05 10:48:10 +0900

    Update BRACKETS2.c

--- BRACKETS2.c
+++ BRACKETS2.c
@@ -1,4 +1,3 @@
-//1번
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -9,96 +8,117 @@
 typedef char element;
 
 typedef struct {
-   element A[MAX_STACK_SIZE];
-   int top;
+    element A[MAX_STACK_SIZE];
+    int top;
 } StackType;
 
 void init_stack(StackType* s)         //스택 초기화 함수
 {
-   s->top = -1;
+    s->top = -1;
 }
 
-int is_empty(StackType *s)
+int is_empty(StackType* s)
 {
-   return (s->top == -1);
+    return (s->top == -1);
 }
 
 int is_full(StackType* s)
 {
-   return (s->top == (MAX_STACK_SIZE - 1));
+    return (s->top == (MAX_STACK_SIZE - 1));
 }
 
 void push(StackType* s, element item)
 {
-   if (is_full(s)) {
-      fprintf(stderr, "스택 포화상태");
-      return;
-   }
-   else s->A[++(s->top)] = item;
+    if (is_full(s)) {
+        fprintf(stderr, "스택 포화상태");
+        return;
+    }
+    else s->A[++(s->top)] = item;
 }
 
 element pop(StackType* s)
 {
-   if (is_empty(s)) {
-      fprintf(stderr, "스택 공백에러");
-      return 0;
-   }
-   else return s->A[(s->top)--];
+    if (is_empty(s)) {
+        fprintf(stderr, "스택 공백에러");
+        return 0;
+    }
+    else return s->A[(s->top)--];
 }
 
 element peek(StackType* s)
 {
-   if (is_empty(s)) {
-      fprintf(stderr, "스택 공백에러");
-      return 0;
-   }
-   else return s->A[(s->top)];
+    if (is_empty(s)) {
+        fprintf(stderr, "스택 공백에러");
+        return 0;
+    }
+    else return s->A[(s->top)];
 }
 
-int checking(const char *in)
+int checking(const char* in)
 {
-   StackType s;
-   char ch, open_ch;
-   int i, n = strlen(in);
+    StackType s;
+    char ch, open_ch;
+    int i, n = strlen(in);
 
-   init_stack(&s);
+    init_stack(&s);
 
-   for (i = 0; i < n; i++) {
-      ch = in[i];
-      switch (ch)
-      {
-      case'(':case'{':case'[':
-         push(&s, ch);
-         break;
+    for (i = 0; i < n; i++) {
+        ch = in[i];
+        switch (ch)
+        {
+        case'(':case'{':case'[':
+            push(&s, ch);
+            break;
 
-      case')':case'}':case']':
-         if (is_empty(&s)) return 0;
-         else {
-            open_ch = pop(&s);
-            if ((open_ch == '(' && ch != ')') ||
-               (open_ch == '{' && ch != '}') ||
-               (open_ch == '[' && ch != ']'))
+        case')':case'}':case']':
+            if (is_empty(&s)) return 0;
+            else {
+                open_ch = pop(&s);
+                if ((open_ch == '(' && ch != ')') ||
+                    (open_ch == '{' && ch != '}') ||
+                    (open_ch == '[' && ch != ']'))
 
-               return 0;
-         }
-         break;
-      }
-   }
-   if (!is_empty(&s)) return 0;
+                    return 0;
+            }
+            break;
+        }
+    }
+    if (!is_empty(&s)) return 0;
 
-   return 1;
+    return 1;
 }
 
 int main()
 {
-   char p[MAX_STACK_SIZE];
-   
-      scanf("%s", &p);
+    int j, C;
+    scanf("%d", &C);
 
-      if (checking(p) == 1)
-         printf("YES");
-      else
-         printf("NO");
-      return 0;
-   
+    for (j = 0; j < C; j++) {
+
+
+        char p[MAX_STACK_SIZE];
+        char q[MAX_STACK_SIZE];
+        char r[MAX_STACK_SIZE];
+
+        scanf("%s", &p);
+        scanf("%s", &q);
+        scanf("%s", &r);
+
+        if (checking(p) == 1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+        if (checking(q) == 1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+
+        if (checking(r) == 1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+        return 0;
+
+    }
 }
