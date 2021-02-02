262d21da119958821789e9d6092ad9714de01adf
TastyAlgorithm <76274621+TastyAlgorithm@users.noreply.github.com>
2021-02-02 09:30:53 +0900

    Create JUMPGAME.c

---
+++ JUMPGAME.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+void init_cache(int(*cache)[101]);
+int jump(int(*cache)[101], int(*map)[101], int x, int y, int n);
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    int C, t;
+    int n, i, j;
+    int map[101][101];
+    int cache[101][101];
+    scanf("%d", &C);
+    for (t = 1; t <= C; t++) {
+
+        init_cache(cache);
+
+        scanf(" %d", &n);
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                scanf(" %d", &map[i][j]);
+            }
+        }
+
+
+        if (jump(cache, map, 0, 0, n) > 0)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
+
+void init_cache(int(*cache)[101])
+{
+    int i, j;
+    for (i = 0; i < 101; i++) {
+        for (j = 0; j < 101; j++) {
+            cache[i][j] = -1;
+        }
+    }
+}
+int jump(int(*cache)[101], int(*map)[101], int x, int y, int n)
+{
+
+    if (x < 0 || y < 0 || x >= n || y >= n)
+        return 0;
+
+    int move = map[x][y];
+    int* ret = &(cache[x][y]);
+
+    if (x == n - 1 && y == n - 1)
+        return 1;
+
+    if (*ret != -1)
+        return *ret;
+
+    *ret = 0;
+
+    *ret = jump(cache, map, x + move, y, n) || jump(cache, map, x, y + move, n);
+
+    return *ret;
+}
